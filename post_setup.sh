95d35051016b7f1a545a3823cdbeab765914add7
Ruu Mouat <ruu1989@me.com>
2016-08-19 17:40:31 +0100

    Make post_setup use mkdir -p to ignore if exists

--- post_setup.sh
+++ post_setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Decrypt private key
-mkdir ~/.ssh; openssl enc -d -aes-256-cbc -in ~/Dropbox/id_rsa.enc -out ~/.ssh/id_rsa
+mkdir -p ~/.ssh
+openssl enc -d -aes-256-cbc -in ~/Dropbox/id_rsa.enc -out ~/.ssh/id_rsa
 chmod 0600 ~/.ssh/id_rsa
 cp ~/Dropbox/id_rsa.pub ~/.ssh
 
